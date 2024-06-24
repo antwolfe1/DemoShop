6650e7b7fdba70beb1b15ca4430a099781f044d6
Wolfe <anwolfe@teksystems.com>
2024-06-24 16:07:54 -0500

    feat: create and test products page

---
+++ application/pokeshop/src/app/products/page.js
@@ -0,0 +1,4 @@
+export default function Products() {
+  return <header className="products-page">Products</header>;
+}
+
---
+++ service/it/SeleniumTests.java
@@ -0,0 +1,37 @@
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumTests {
+
+
+    @Test
+    void getInventoryPageTitle() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://localhost:3000/products");
+        WebElement title = driver.findElement(By.className("products-page"));
+        assertEquals("Products", title.getText());
+        driver.close();
+    }
+
+//    @Test
+//    void getProductsOnInventoryPage() {
+//        driver = PokeshopClient().getWebDriver();
+//        InventoryRepository
+//        InventoryView inventoryView = driver.goTo();
+//        List<Product> actualProducts = inventoryView.getProductList();
+//        assertEqual();
+//    }
+}
