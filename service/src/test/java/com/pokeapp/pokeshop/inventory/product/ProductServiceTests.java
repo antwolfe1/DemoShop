9d7ec14cc51107f7dcd0b314972a86792c58f971
anwolfe <anwolfe@teksystems.com>
2024-06-26 12:07:42 -0500

    test: add unfinished tests

--- service/src/test/java/com/pokeapp/pokeshop/inventory/product/ProductServiceTests.java
+++ service/src/test/java/com/pokeapp/pokeshop/inventory/product/ProductServiceTests.java
@@ -1,18 +1,25 @@
 package com.pokeapp.pokeshop.inventory.product;
 
-import com.google.gson.JsonObject;
-import org.json.JSONObject;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductServiceTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void transformDataToJSON(){
-        String testJsonString = "{\"test\": \"words\", \"case\": \"options\"}";
-        ProductService productService = ProductService.createInstance();
-        assertEquals(JsonObject.class, productService.stringToJson(testJsonString).getClass());
-        assertEquals("options", productService.stringToJson(testJsonString).get("case").getAsString());
+    void getProducts() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 }
