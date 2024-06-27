91f315380bd1225423d588102a8a559b4ba8a67a
anwolfe <anwolfe@teksystems.com>
2024-06-27 15:27:03 -0500

    chore: remove unused variables

--- application/pokeshop/test/app/products/productView.test.js
+++ application/pokeshop/test/app/products/productView.test.js
@@ -2,14 +2,12 @@ const getProducts = require('../../../src/app/products/productView');
 
 
 test('getProducts returns non-empty array of objects', async () => {
-    expectedArray = [];
     let actualProducts = await getProducts();
     expect(actualProducts.length).toBeGreaterThan(0);
     expect(actualProducts[0].name).toBe('bulbasaur')
 })
 
 test('getProducts returns "bulbasaur" as first entry', async () => {
-    expectedArray = [];
     let actualProducts = await getProducts();
     expect(actualProducts[0].name).toBe('bulbasaur')
 })
